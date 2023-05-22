82e369ce316ebad9e59f654c3bd3ad1f84286ad3
Nik Peric <nik@nikperic.com>
2023-05-22 06:59:32 +0200

    fix a few edge cases, add logging to dev/test, write some notes for later

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # route-craft
 
 WIP experiments in route generation based on PostgreSQL schema. Not ready for any real use.
+
+## Rationale
+
+## Usage
+
+### Configuration Options
+
+### Supported handlers
+
+- `:insert-one` => `POST /`
+- `:get-by-pk` => `GET /:{pk}`
+- `:update-by-pk` => `PUT /:{pk}`
+- `:delete-by-pk` => `DELETE /:{pk}`
+
+#### Planned handlers
+
+- `:get-many` => `GET /`
+- `:update-by-query` => `PUT /`
+- `:delete-by-query` => `DELETE /`
+
+#### Rough ideas for handlers
+
+- `:insert-many` => `POST /` with header?
+- `:upsert-by-pk` => `PUT /:{pk}` with header?
+
+## Auth and Permissions
+
+## Query Parameters
+
+## When not to use this
+
+- Very high throughput APIs
+  - There is overhead with permission checking and other things that could be avoided with a more direct approach
+- More complex handlers should be managed via custom requests
+  - Goal of route-craft is to handle the "80%" case of CRUD operations on database tables while permitting extensibility for the rest
+
+## License
+
+Copyright © 2023
+
+Released under the MIT license.
--- src/route_craft/core.clj
+++ src/route_craft/core.clj
@@ -180,7 +180,7 @@
     (if (= 1 (count pk-cols))
       (first pk-cols)
       (throw (ex-info "Unexpected primary key count, please define manually" {:count (count pk-cols)
-                                                                              :type ::unexpected-pk-count})))))
+                                                                              :type  ::unexpected-pk-count})))))
 
 (defn table->reitit-routes
   [{:keys [table-definitions default-handlers throw-on-failure? malli-type-mappings]
@@ -190,33 +190,35 @@
    table]
   (let [table-opts (get table-definitions table)]
     (when-not (:ignore? table-opts)
-      (try (let [dbtable            (get tables table)
-                 pk-key             (or (:pk-key table-opts) (get-pk-from-table dbtable))
-                 pk-key-path        (str "/" pk-key)
-                 generate-handler-fn (partial generate-handler {:table-name          table
-                                                               :table               dbtable
-                                                               :pk-key              pk-key
-                                                               :malli-type-mappings malli-type-mappings})]
-             (->> (reduce
-                    (fn [out handler]
-                      (let [path (case handler
-                                   :insert-one ["" :post]
-                                   :get-by-pk [pk-key-path :get]
-                                   :update-by-pk [pk-key-path :put]
-                                   :delete-by-pk [pk-key-path :delete]
-                                   (throw (ex-info "Unsupported handler" {:type    ::unsupported-handler
-                                                                          :handler handler})))]
-                        (assoc-in out path (generate-handler-fn handler))))
-                    {}
-                    (get-in table-definitions [table :handlers] default-handlers))
-                  (vec)
-                  (cons (str "/" (name table)))
-                  (vec)))
-           (catch Exception e
-             (log/trace e "Route generation exception")
-             (log/warn "Failed to generate routes for table" table)
-             (when throw-on-failure?
-               (throw e)))))))
+      (let [handlers (get-in table-definitions [table :handlers] default-handlers)]
+        (when (seq handlers)
+          (try (let [dbtable             (get tables table)
+                     pk-key              (or (:pk-key table-opts) (get-pk-from-table dbtable))
+                     pk-key-path         (str "/" pk-key)
+                     generate-handler-fn (partial generate-handler {:table-name          table
+                                                                    :table               dbtable
+                                                                    :pk-key              pk-key
+                                                                    :malli-type-mappings malli-type-mappings})]
+                 (->> (reduce
+                        (fn [out handler]
+                          (let [path (case handler
+                                       :insert-one ["" :post]
+                                       :get-by-pk [pk-key-path :get]
+                                       :update-by-pk [pk-key-path :put]
+                                       :delete-by-pk [pk-key-path :delete]
+                                       (throw (ex-info "Unsupported handler" {:type    ::unsupported-handler
+                                                                              :handler handler})))]
+                            (assoc-in out path (generate-handler-fn handler))))
+                        {}
+                        handlers)
+                      (vec)
+                      (cons (str "/" (name table)))
+                      (vec)))
+               (catch Exception e
+                 (log/trace e "Route generation exception")
+                 (log/warn "Failed to generate routes for table" table)
+                 (when throw-on-failure?
+                   (throw e)))))))))
 
 (defn routes-from-dbxray
   [opts {:keys [table-order] :as db-xray}]
